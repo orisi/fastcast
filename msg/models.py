1583de6707cb3481587bb8267422425b370b5ed4
Kuba Kucharski <kuba@Kubas-MacBook-Air.local>
2014-07-29 15:43:07 +0200

    changed models

--- msg/models.py
+++ msg/models.py
@@ -5,17 +5,12 @@ class Msg( models.Model ):
     """
     Model for storing `messages`
     """
-   
-    subject = models.CharField( max_length = 100 )
-    origin = models.CharField( max_length = 100 )
-    channel = models.CharField( max_length = 100 )
+
     message_id = models.CharField( max_length = 100 )
-    pubkey_list = models.CharField( max_length = 100 )
-    miners_fee_satoshi = models.CharField( max_length = 100 )
-    prevtxs = models.CharField( max_length = 100 )
-    outputs = models.CharField( max_length = 100 )
-    req_sigs = models.CharField( max_length = 100 )
-    operation = models.CharField( max_length = 100 )
-    sum_satoshi = models.CharField( max_length = 100 )
-    locktime = models.CharField( max_length = 100 )
+    timestamp = models.DateTimeField()
+    source = models.CharField( max_length = 100 )
+    destination = models.CharField( max_length = 100 )
+    channel = models.CharField( max_length = 100 )
+    signature = models.CharField( max_length = 100 )
+    body = models.BinaryField()
 
--- msg/serializers.py
+++ msg/serializers.py
@@ -1,12 +1,10 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups')
-
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -18,6 +16,6 @@ from msg.models import Msg
 class MsgSerializer( serializers.ModelSerializer ):
     class Meta:
         model = Msg
-        fields = ( 'subject','origin','channel','message_id','pubkey_list','miners_fee_satoshi','prevtxs','outputs','req_sigs','operation','sum_satoshi','locktime' )
+        fields = ( 'message_id','timestamp','source','destination','channel','signature','body')
 
 
