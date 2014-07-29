336651fd171c133e7628479d3cf1ac7e93f4e7ef
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-07-29 20:29:07 +0200

    Update models.py

--- msg/models.py
+++ msg/models.py
@@ -1,5 +1,35 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+
+import uuid # from http://zesty.ca/python/uuid.html
+import sys
+import base64 
+ 
+ 
+def fetch_code(custom_string="CODE_'):
+    """
+    takes an optional argument, a string to be put at the beginning of a code,
+    so the code will look like that:
+ 
+    CODE_8mANDJ
+    KEY_9coOeC
+ 
+    default is CODE_
+ 
+    usage:
+    fetch_code()   
+    fetch_code(custom_string="KEY_")
+  
+    """
+    b64uid = '00000000'
+    
+   
+    uid = uuid.uuid4()
+    b64uid = base64.b64encode(uid.bytes,'-_')
+    
+    code = b64uid[0:6]
+    return custom_string+code
+
 # Create your models here.
 class Msg( models.Model ):
     """
@@ -22,4 +52,5 @@ class Msg( models.Model ):
     channel = models.CharField( max_length = 100 )
     signature = models.CharField( max_length = 100 )
     body = models.FileField(upload_to="messages/%Y/%m/%d/", validators=[validate_file])
+
 
