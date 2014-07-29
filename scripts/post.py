2c6a32543cfe8e4c4a13aa5469d6edbc87fbebd4
Kuba Kucharski <kuba@Kubas-MacBook-Air.local>
2014-07-29 23:14:21 +0200

    new scripts

---
+++ scripts/post.py
@@ -0,0 +1,27 @@
+import requests
+url = 'http://54.77.58.8/'
+
+payload = {
+
+            "source": "1",
+            "destination": "1",
+            "channel": "1",
+            "signature": "1",
+            "body": "nope",
+
+
+}
+
+
+data_x = { "Test" : "test" }
+
+### Create file and open
+#import json
+#Bwith open('test.json', 'w') as outfile:
+#  json.dump(data_x, outfile)
+
+files = {'file': open('test.json', 'rb')}
+r = requests.post(url, files=files, data=payload)
+
+print r.text
+print r.headers
