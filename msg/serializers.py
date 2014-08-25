13dd46759c48b07e087c1fbc310cf76699410665
Kuba Kucharski <kuba.kucharski@gmail.com>
2014-08-25 13:25:43 +0200

    Update serializers.py

--- msg/serializers.py
+++ msg/serializers.py
@@ -1,19 +1,7 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class UnixEpochDateField(serializers.DateTimeField):
-    def to_native(self, value):
-        """ Return epoch time for a datetime object or ``None``"""
-        import time
-        try:
-            return int(time.mktime(value.timetuple()))
-        except (AttributeError, TypeError):
-            return None
 
-    def from_native(self, value):
-        import datetime
-        return datetime.datetime.fromtimestamp(int(value))
-        
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
