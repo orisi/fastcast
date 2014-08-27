ce100962b3adf1c9d83e165f5bbe18e5e061d8b4
Grzegorz Pstrucha <gpstrucha@gmail.com>
2014-08-28 01:08:42 +0200

    Verify all messages at server side

--- msg/serializers.py
+++ msg/serializers.py
@@ -1,7 +1,23 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
 
+import base64
+import binascii
+
+def verify(message, signature_b64, pub_b64):
+  pub = base64.decodestring(pub_b64)
+  signature = base64.decodestring(signature_b64)
+  key = RSA.importKey(pub)
+  signer = PKCS1_v1_5.new(key)
+  digest = SHA256.new()
+  digest.update(message)
+  if signer.verify(digest, signature):
+    return True
+  return False
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -16,10 +32,23 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 from msg.models import Msg
 
 class MsgSerializer( serializers.ModelSerializer ):
-   
-        
     class Meta:
         model = Msg
         fields = ('frame_id','timestamp','source','channel','signature','body','epoch')
         read_only_fields = ('epoch',)
- 
+
+    def validate(self, attrs):
+      try:
+        body = base64.decodestring(attrs['body'])
+      except binascii.Error:
+        raise serializers.ValidationError("Body must be base64 encoded string")
+
+      try:
+        verified = verify(body, attrs['signature'], attrs['source'])
+      except binascii.Error:
+        verified = False
+
+      if not verified:
+        raise serializers.ValidationError("The message is not signed by proper signature")
+
+      return attrs
